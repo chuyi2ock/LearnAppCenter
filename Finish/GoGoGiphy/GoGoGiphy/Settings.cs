9aaca358b51fb6d2009ed20ea95ead15a5dc87bf
Jesus Hernandez <jesus.hernandez@unosquare.com>
2020-03-02 16:02:51 -0600

    Update Versions

--- Finish/GoGoGiphy/GoGoGiphy/Settings.cs
+++ Finish/GoGoGiphy/GoGoGiphy/Settings.cs
@@ -10,17 +10,17 @@ namespace GoGoGiphy.Core
         internal static string AppName { get; set; } = "GoGoGiphy";
 
         // TODO: Add Secret value for iOS app here.
-        internal static string AppCenterSecretiOS { get; set; } = "iOSSecret";
+        internal static string AppCenterSecretiOS { get; set; } = "8a8f7259-1d7e-4eca-98d6-55406943f4ac";
 
         // TODO: Add Secret value for Android app here.
-        internal static string AppCenterSecretAndroid { get; set; } = "AndroidSecret";
+        internal static string AppCenterSecretAndroid { get; set; } = "e746cd72-b2f6-4d45-94f7-2f10bf88793e";
 
         internal static string SearchUrl { get; set; } = @"https://api.giphy.com/v1/gifs/search?api_key={0}&q={1}&limit={2}&offset={3}&rating={4}&lang=en";
 
         internal static string TrendingUrl { get; set; } = @"https://api.giphy.com/v1/gifs/trending?api_key={0}&limit={1}&offset={2}&rating={3}";
 
         // TODO: Add key for Giphy API here.
-        internal static string ApiKey { get; } = "GiphyApiKey";
+        internal static string ApiKey { get; } = "Zgvgxsjj9GNkdzC0xTxF38ZSl8cAUDVl";
 
         internal static int LimitCount { get; set; } = 30;
 
